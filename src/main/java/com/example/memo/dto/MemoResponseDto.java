a829602bb47229fa4b149e5b30f981357bcd8c49
Kim Taejin <rlaxowls0504@gmail.com>
2024-04-10 01:49:06 +0900

    feat: memo responst dto 생성

---
+++ src/main/java/com/example/memo/dto/MemoResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.memo.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoResponseDto {
+    private Long id;
+    private String userName;
+    private String contents;
+}
