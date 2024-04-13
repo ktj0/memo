4b1a19dd1d56ab33f61508b8bd6a09b8bb7048e5
Kim Taejin <rlaxowls0504@gmail.com>
2024-04-14 01:29:32 +0900

    refactor: create db로 구현

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -3,8 +3,13 @@ package com.example.memo.controller;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -13,49 +18,64 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class MemoController {
-    // DB가 없기 때문에 메모를 저장할 map
-    private final Map<Long, Memo> memoList = new HashMap<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemoController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
         Memo memo = new Memo(requestDto);
 
-        Long maxId = !memoList.isEmpty() ? Collections.max(memoList.keySet()) + 1 : 1;
-        memo.setId(maxId);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
-        memoList.put(memo.getId(), memo);
+        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
+        jdbcTemplate.update(con -> {
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-        return new MemoResponseDto(memo);
+            preparedStatement.setString(1, memo.getUsername());
+            preparedStatement.setString(2, memo.getContents());
+
+            return preparedStatement;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+        memo.setId(id);
+
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+
+        return memoResponseDto;
     }
 
-    @GetMapping("/memos")
-    public List<MemoResponseDto> getMemos() {
-        List<MemoResponseDto> responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
-
-        return responseList;
-    }
-
-    @PutMapping("/memos/{id}")
-    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-        if (memoList.containsKey(id)) {
-            Memo memo = memoList.get(id);
-
-            memo.update(requestDto);
-
-            return memo.getId();
-        } else {
-            throw new IllegalArgumentException("선택된 메모는 존재하지 않습니다.");
-        }
-    }
-
-    @DeleteMapping("memos/{id}")
-    public Long deleteMemo(@PathVariable Long id) {
-        if (memoList.containsKey(id)) {
-            memoList.remove(id);
-
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택된 메모는 존재하지 않습니다.");
-        }
-    }
+//    @GetMapping("/memos")
+//    public List<MemoResponseDto> getMemos() {
+//        List<MemoResponseDto> responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
+//
+//        return responseList;
+//    }
+//
+//    @PutMapping("/memos/{id}")
+//    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+//        if (memoList.containsKey(id)) {
+//            Memo memo = memoList.get(id);
+//
+//            memo.update(requestDto);
+//
+//            return memo.getId();
+//        } else {
+//            throw new IllegalArgumentException("선택된 메모는 존재하지 않습니다.");
+//        }
+//    }
+//
+//    @DeleteMapping("memos/{id}")
+//    public Long deleteMemo(@PathVariable Long id) {
+//        if (memoList.containsKey(id)) {
+//            memoList.remove(id);
+//
+//            return id;
+//        } else {
+//            throw new IllegalArgumentException("선택된 메모는 존재하지 않습니다.");
+//        }
+//    }
 }
