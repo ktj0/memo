e4e125bc1adf79d18ef16d1e8a01d40f46c1b5ce
Kim Taejin <rlaxowls0504@gmail.com>
2024-04-18 01:59:03 +0900

    refactor: 메모 조회 query method를 사용해서 수정 날짜 기준 내림차순으로 변경

--- src/main/java/com/example/memo/repository/MemoRepository.java
+++ src/main/java/com/example/memo/repository/MemoRepository.java
@@ -4,5 +4,9 @@ import com.example.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface MemoRepository extends JpaRepository<Memo, Long> {}
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+}
